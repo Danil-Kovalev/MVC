d1018454ea1f1f1bf60dd03413511dd52959aaec
Kowalski <danilkovalev1306@gmail.com>
2024-12-14 14:32:10 +0000

    Update config db file

--- MVC/dist/src/back/config/config.js
+++ MVC/dist/src/back/config/config.js
@@ -2,9 +2,9 @@
  * Config for connect to database
  */
 export const CONFIG = {
-    host: "127.0.0.1",
+    host: process.env.MYSQL_HOST || "127.0.0.1",
     port: 3306,
-    user: "ubuntu",
-    database: "dataBooks",
-    password: ""
+    user: process.env.MYSQL_USER || "ubuntu",
+    database: process.env.MYSQL_DATABASE || "dataBooks",
+    password: process.env.MYSQL_PASSWORD || ""
 };
--- MVC/dist/src/back/src/serverV1.js
+++ MVC/dist/src/back/src/serverV1.js
@@ -36,7 +36,6 @@ app.set('views', path.join(dirname, '/views'));
  * Main page with books
  */
 app.get('/', (req, res) => {
-    console.log("It works!");
     res.render(path.join(dirname, '/views/books-page.ejs'));
 });
 /**
